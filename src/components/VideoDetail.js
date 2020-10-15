39772185ac3b8c9cd23fb5669b06b792fa7fa402
Roksana Dzioba <roksanadzioba@gmail.com>
2020-10-16 00:36:19 +0200

    add video snippet

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,13 +4,19 @@ import { Paper, Typography } from '@material-ui/core'
 const VideoDetail = ({video}) => {
 
     if(!video) return <div>Loading...</div>
+    console.log(video)
+    
     const VideoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     return(
         <React.Fragment>
             <Paper elevation={6} style={{height:'70%'}}>
                 <iframe frameBorder="0" height="100%" width="100%" title="Video Player" src={VideoSrc} />
             </Paper>
-            <Paper elevation={6} style={{padding:'15px'}}></Paper>
+            <Paper elevation={6} style={{padding:'15px'}}>
+                <Typography variant="h4">{video.snippet.tittle} - {video.snippet.chanelTittle}</Typography>
+                <Typography variant="subtitle1">{video.snippet.chanelTittle}</Typography>
+                <Typography variant="subtitle2">{video.snippet.description}</Typography>
+            </Paper>
         </React.Fragment>
         
     )
