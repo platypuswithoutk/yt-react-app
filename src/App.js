493248ce8adfbe0ceb001be9c8b9cb66b4bd97f2
Roksana Dzioba <roksanadzioba@gmail.com>
2020-10-15 22:25:02 +0200

    add axios and grid system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import youtube from './api/youtube';
+import { SerachBar, VideoList, VideoDetail } from './components';
 
 class App extends React.Component {
     render() {
         return(
-            <h1>YouTube App</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacink={16}>
+                        <Grid item xs={12}>
+                        </Grid>
+                        <Grid item xs={8}></Grid>
+                        <Grid item xs={4}></Grid>
+                    </Grid>   
+                </Grid>
+            </Grid>
         )
     }
 }
