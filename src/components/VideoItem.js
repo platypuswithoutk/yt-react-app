004817981e0f5f9ee1c2635bab3c61a51d600400
Roksana Dzioba <roksanadzioba@gmail.com>
2020-10-18 22:48:26 +0200

    add font-awesome dependencies

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Grid, Paper, Typography } from '@material-ui/core';
+import '../style/VideoItemStyle.css'
 
 const VideoItem = ({video, onVideoSelect }) => {
     return(
         <Grid item xs={12}>
-            <Paper style={{display: 'flex', alignItems: 'center', cursor: 'pointer'}} onClick={() => onVideoSelect(video)}>
-                <img  style={{marginRight: '20px'}} alt="images" src={video.snippet.thumbnails.medium.url}/>
+            <Paper className="video-item-paper" onClick={() => onVideoSelect(video)}>
+                <img className="video-item-img" alt="images" src={video.snippet.thumbnails.medium.url}/>
                 <Typography variant="subtitle1"><b>{video.snippet.title}</b></Typography>
             </Paper>
 
